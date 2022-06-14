0fd9269d47abbb7b25fc289e5e1be64167ed47b7
Lee Macintyre <macintyrelee@gmail.com>
2022-06-14 06:20:20 +0100

    created tab component

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -3,9 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AuthModalComponent } from './auth-modal/auth-modal.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [AuthModalComponent],
+  declarations: [AuthModalComponent, LoginComponent, RegisterComponent],
   imports: [CommonModule, SharedModule],
   exports: [AuthModalComponent, SharedModule],
 })
