4ebd8135a94861962fe1cb29e1f28e83aaac1e27
Lee Macintyre <macintyrelee@gmail.com>
2022-06-23 08:29:40 +0100

    added validation

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
+import IUser from 'src/app/models/user.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { RegisterValidators } from '../validators/register-validators';
+import { EmailTaken } from '../validators/email-taken';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,17 @@ import { FormGroup, FormControl, Validator, Validators } from '@angular/forms';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent {
+  constructor(private auth: AuthService, private emailTaken: EmailTaken) {}
+
+  inSubmission = false;
+
   name = new FormControl('', [Validators.required, Validators.minLength(3)]);
-  email = new FormControl('', [Validators.required, Validators.email]);
-  age = new FormControl('', [
+  email = new FormControl(
+    '',
+    [Validators.required, Validators.email],
+    [this.emailTaken.validate]
+  );
+  age = new FormControl(null, [
     Validators.required,
     Validators.min(18),
     Validators.max(120),
@@ -29,18 +41,34 @@ export class RegisterComponent {
   alertMsg = 'Please wait! Your Account is being created.';
   alertColor = 'blue';
 
-  registerForm = new FormGroup({
-    name: this.name,
-    email: this.email,
-    age: this.age,
-    password: this.password,
-    confirm_password: this.confirm_password,
-    phoneNumber: this.phoneNumber,
-  });
+  registerForm = new FormGroup(
+    {
+      name: this.name,
+      email: this.email,
+      age: this.age,
+      password: this.password,
+      confirm_password: this.confirm_password,
+      phoneNumber: this.phoneNumber,
+    },
+    [RegisterValidators.match('password', 'confirm_password')]
+  );
 
-  register() {
+  async register() {
     this.showAlert = true;
     this.alertMsg = 'Please wait! Your Account is being created.';
     this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    try {
+      this.auth.createUser(this.registerForm.value as IUser);
+    } catch (error) {
+      console.log(error);
+      this.alertMsg = 'An unexpected error occurred. please try again later';
+      this.alertColor = 'red';
+      this.inSubmission = false;
+      return;
+    }
+    this.alertMsg = 'Success! your account has been created';
+    this.alertColor = 'green';
   }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyD0TEgoXbK_P8dn8iI5iS3J4IyV0lcqLO4',
+    authDomain: 'clips-17d25.firebaseapp.com',
+    projectId: 'clips-17d25',
+    storageBucket: 'clips-17d25.appspot.com',
+    messagingSenderId: '961911186582',
+    appId: '1:961911186582:web:8ba865fe7f0638bec64eb7',
+  },
 };
 
 /*
